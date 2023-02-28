c28a2e525ca9b00931a0b3790ba46603f9a8f315
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-02-28 16:15:18 +0100

    Created a specific file containing the infos for the Types model

---
+++ config/helpers/types.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    [
+        "name"=>"Front-End",
+        "vanilla"=>false,
+    ],
+    
+    [
+        "name"=>"Front-End",
+        "vanilla"=>true,
+    ],
+    
+    [
+        "name"=>"Back-End",
+        "vanilla"=>false,
+    ],
+    
+    [
+        "name"=>"Back-End",
+        "vanilla"=>true,
+    ],
+];
